43e48738f5576411961663a29b513b32070bf828
HassanKhan123 <hk260280@gmail.com>
2025-12-01 18:38:47 +0100

    implement hoc for managing window states and terminal window added

---
+++ src/components/WindowControls.jsx
@@ -0,0 +1,14 @@
+import useWindowStore from "#store/window";
+import React from "react";
+
+const WindowControls = ({ target }) => {
+  const { closeWindow } = useWindowStore();
+
+  return (
+    <div id="window-controls">
+      <div className="close" onClick={() => closeWindow(target)} />
+    </div>
+  );
+};
+
+export default WindowControls;
