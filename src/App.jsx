7d0cfda16d45880dd394b976a12e2906672f59a2
HassanKhan123 <hk260280@gmail.com>
2025-12-01 14:17:27 +0100

    added: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from "#components/Navbar";
 import React from "react";
 
 const App = () => {
-  return <div className="font-bold">App</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { navLinks } from "#constants";
+import dayjs from "dayjs";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Hassan's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
